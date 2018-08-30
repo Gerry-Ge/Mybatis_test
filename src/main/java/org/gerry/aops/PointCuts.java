8f9e0087b5256b4a392a5ff32aaff26fe75a8c84
Gerry-Ge <18552132787@163.com>
2018-08-30 14:31:43 +0800

    single file commit;

--- src/main/java/org/gerry/aops/PointCuts.java
+++ src/main/java/org/gerry/aops/PointCuts.java
@@ -18,24 +18,4 @@ public class PointCuts {
 	}
 
 }
-=======
-package org.gerry.aops;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
-import org.gerry.main.CodeUtils;
-
-@Aspect
-public class PointCuts {
-	@Pointcut("Within(org.gerry.test)")
-	public void withIn(){
-		
-	}
-	
-	@Before("withIn()")
-	public void before(){
-		CodeUtils.prt("#Before#");
-	}
-
-}
